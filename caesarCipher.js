6726ba5d3903d813ea1a051016886ae617596d3d
Srr233 <Sr233@yandex.ru>
2021-05-07 13:35:52 +0300

    feat: add many things: coding, checking etc...

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,9 +1,18 @@
-const { writeFile, isAllOptionExists, areCorrectValues } = require('./services');
+const { isAllOptionExists, areCorrectValues, doCaesarCipher } = require('./services');
+
+process.on('exit', (code) => {
+  return console.log(`Process exit with code: ${code}`);
+});
+
 
 async function init (options) {
   const checkExists = isAllOptionExists(options);
   if (checkExists) {
-    const checkValues = areCorrectValues(options);
+    const areCorrect = areCorrectValues(options);
+
+    if (areCorrect) {
+      doCaesarCipher(options);
+    }
   } else {
     throw new Error('all keys should exist');
   }
--- services.js
+++ services.js
@@ -1,24 +1,33 @@
 const fs = require('fs').promises;
 
+const readFile = async (path) => {
+  try {
+    const data = await fs.readFile(path);
+    return data.toString();
+  } catch (e) {
+    await fs.appendFile('./stderr.txt', `\n-----${new Date()}: Can't read file\nCurrent error:\n${e}`);
+    throw new Error('Ops! Try to find out this error in the stderr.txt!');
+  }
+}
 const writeFile = async (path, text) => {
   try {
     await fs.writeFile(path, text);
     return true;
   } catch (e) {
-    console.log(e);
-    return false;
+    await fs.appendFile('./stderr.txt', `\n-----${new Date()}: Can't write file\nCurrent error:\n${e}`);
+    throw new Error('Ops! Try to find out this error in the stderr.txt!');
   }
 }
 
+const allOptions = ['action', 'shift'];
 
-
-
-const allOptions = ['action', 'shift', 'input', 'output'];
-
-const isAllOptionExists = (options) => allOptions.every((item) => options[item]);
+const isAllOptionExists = (options) => allOptions.every((item) => {
+  if (!options[item]) process.exit(1);
+  return true;
+});
 
 const areCorrectValues = (options) => {
-  return allOptions.every((item) => {
+  const resultOfEvery = allOptions.every((item) => {
     let res;
     switch(item) {
       case 'action':
@@ -31,20 +40,61 @@ const areCorrectValues = (options) => {
         res = !isNaN(+options[item]);
         if (!res) throw new Error('shift should be a number');
         return res;
-
-      case 'input':
-        res = options[item].includes('.txt');
-        if (!res) throw new Error('input file should have a txt type');
-        return res;
-      case 'output':
-        res = options[item].includes('.txt');
-        if (!res) throw new Error('output file should have a txt type');
-        return res;
     }
   });
+  return resultOfEvery;
+}
+
+const doCaesarCipher = async (options) => {
+
+  const input = options.input ? options.input : './stdin.txt';
+  const output = options.output ? options.output : './stdout.txt';
+  const infoOfInput = await readFile(input);
+
+  const doCode = (text, shift) => {
+    const C_LOWER_S = 97;
+    const C_LOWER_E = 122;
+    const C_UPPER_S = 65;
+    const C_UPPER_E = 90;
+    let result = '';
+    for (const letter of text) {
+      const l = letter.charCodeAt();
+      if (l >= C_UPPER_S && l <= C_UPPER_E) {
+        const shifted = l + +shift;
+        const newPos = shifted === C_UPPER_S ? shifted
+          : shifted < C_UPPER_S ? C_UPPER_E - (C_UPPER_S - shifted)
+            : shifted === C_UPPER_E ? shifted
+              : shifted > C_UPPER_E ? C_UPPER_S + (shifted - C_UPPER_E)
+                : shifted;
+        result += String.fromCharCode(newPos);
+      } else if (l >= C_LOWER_S && l <= C_LOWER_E) {
+        const shifted = l + +shift;
+        const newPos = shifted === C_LOWER_S ? shifted
+          : shifted < C_LOWER_S ? C_LOWER_E - (C_LOWER_S - shifted)
+            : shifted === C_LOWER_E ? shifted
+              : shifted > C_LOWER_E ? C_LOWER_S + (shifted - C_LOWER_E)
+                : shifted;
+        result += String.fromCharCode(newPos);
+      } else {
+        result += letter;
+      }
+    }
+    return result;
+  }
+  let newText = '';
+  switch(options.action) {
+    case 'encode':
+      newText = doCode(infoOfInput, options.shift);
+      console.log(newText);
+      break;
+    case 'decode':
+      newText = doCode(infoOfInput, options.shift - options.shift * 2);
+      console.log(newText);
+      break;
+  }
 }
 module.exports = {
-  writeFile,
   isAllOptionExists,
-  areCorrectValues
+  areCorrectValues,
+  doCaesarCipher
 }
